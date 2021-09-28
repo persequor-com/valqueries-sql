5f738d0215063276c7668ab214e7abc6675832c1
anton <anton.larsen@persequor.com>
2021-09-28 05:22:26 +0200

    removed unused debug code

--- test/src/main/java/com/valqueries/DataSourceProvider.java
+++ test/src/main/java/com/valqueries/DataSourceProvider.java
@@ -31,8 +31,6 @@ import java.util.Properties;
 import java.util.concurrent.Executor;
 import java.util.logging.Logger;
 
-import static org.mockito.Mockito.*;
-
 public class DataSourceProvider extends HikariDataSource {
 	private static DataSourceProvider INSTANCE;
 	private final HikariDataSource dataSource;
@@ -51,67 +49,6 @@ public class DataSourceProvider extends HikariDataSource {
 
 	public DataSource getDataSource() {
 		return dataSource;
-//		return new DataSource() {
-//			@Override
-//			public Connection getConnection() throws SQLException {
-//				return wrap(dataSource.getConnection());
-//			}
-//
-//			@Override
-//			public Connection getConnection(String s, String s1) throws SQLException {
-//				return wrap(dataSource.getConnection(s, s1));
-//			}
-//
-//			private Connection wrap(Connection connection) {
-//				System.out.println("open: "+System.identityHashCode(connection));
-//				Connection s = spy(connection);
-//				try {
-//					doAnswer((i) -> {
-//						System.out.println("close: "+System.identityHashCode(connection));
-//						return i.callRealMethod();
-//					}).when(s).close();
-//				} catch (SQLException throwables) {
-//					throw new RuntimeException(throwables);
-//				}
-//				return s;
-//			}
-//
-//			@Override
-//			public PrintWriter getLogWriter() throws SQLException {
-//				return dataSource.getLogWriter();
-//			}
-//
-//			@Override
-//			public void setLogWriter(PrintWriter printWriter) throws SQLException {
-//				dataSource.setLogWriter(printWriter);
-//			}
-//
-//			@Override
-//			public void setLoginTimeout(int i) throws SQLException {
-//				dataSource.setLoginTimeout(i);
-//			}
-//
-//			@Override
-//			public int getLoginTimeout() throws SQLException {
-//				return dataSource.getLoginTimeout();
-//			}
-//
-//			@Override
-//			public <T> T unwrap(Class<T> aClass) throws SQLException {
-//				return dataSource.unwrap(aClass);
-//			}
-//
-//			@Override
-//			public boolean isWrapperFor(Class<?> aClass) throws SQLException {
-//				return dataSource.isWrapperFor(aClass);
-//			}
-//
-//			@Override
-//			public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-//				return dataSource.getParentLogger();
-//			}
-//		};
-//		return dataSource;
 	}
 
 	public static DataSource get() {
