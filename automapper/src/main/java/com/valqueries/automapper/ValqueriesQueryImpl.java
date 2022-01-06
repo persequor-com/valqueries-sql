0d4546965a48d319bc76f32b1d3f6504f021eb52
Teofilo Monteiro <teofilo.monteiro@persequor.com>
2022-01-06 11:06:53 +0100

    bug-fix: eager loading missing and statment between matching columns

--- automapper/src/main/java/com/valqueries/automapper/ValqueriesQueryImpl.java
+++ automapper/src/main/java/com/valqueries/automapper/ValqueriesQueryImpl.java
@@ -168,9 +168,12 @@ public class ValqueriesQueryImpl<T> extends BaseValqueriesQuery<T> implements Va
 			eagerJoin.append(" LEFT JOIN `"+eagerTable+"` "+eagerAlias+" ON ");
 			List<KeySet.Field> from = relation.getFromKeys().stream().collect(Collectors.toList());
 			List<KeySet.Field> to = relation.getToKeys().stream().collect(Collectors.toList());
+			List<String> onParams = new ArrayList<>();
 			for(int x=0;x<from.size();x++) {
-				eagerJoin.append(tableAlias+"."+sqlNameFormatter.column(from.get(x).getToken())+" = "+eagerAlias+"."+sqlNameFormatter.column(to.get(x).getToken()));
+				onParams.add(tableAlias+"."+sqlNameFormatter.column(from.get(x).getToken())+" = "+eagerAlias+"."+sqlNameFormatter.column(to.get(x).getToken()));
 			}
+
+			eagerJoin.append(String.join(" AND ", onParams));
 			TypeDescriber<?> eagerRelationTypeDescriber = TypeDescriberImpl.getTypeDescriber(relation.getToClass().clazz);
 
 			eagerSelect.append(", "+eagerRelationTypeDescriber.fields().stream().map(property -> eagerAlias+"."+sqlNameFormatter.column(property.getToken())+" "+eagerAlias+"_"+sqlNameFormatter.column(property.getToken())).collect(Collectors.joining(", ")));
