7257abf291cfdc3c7b2336b9d48c4093dbe0d98a
anton <anton.larsen@psqr.eu>
2022-04-15 20:13:17 +0200

    Fixed bug in via relations, when multiple via relations pointing to and from the same tables exists between two objects

---
+++ valqueries-test/src/test/java/com/valqueries/automapper/PersonMarriage.java
@@ -0,0 +1,49 @@
+package com.valqueries.automapper;
+
+import io.ran.Mapper;
+import io.ran.PrimaryKey;
+import io.ran.Relation;
+
+@Mapper(dbType = Valqueries.class)
+public class PersonMarriage {
+    @PrimaryKey
+    private String personId;
+    @PrimaryKey
+    private String marriageId;
+    @Relation(fields = "personId", relationFields = "id")
+    private Person person;
+    @Relation(fields = "marriageId", relationFields = "id", autoSave = true)
+    private Marriage marriage;
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getMarriageId() {
+        return marriageId;
+    }
+
+    public void setMarriageId(String marriageId) {
+        this.marriageId = marriageId;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public Marriage getMarriage() {
+        return marriage;
+    }
+
+    public void setMarriage(Marriage marriage) {
+        this.marriage = marriage;
+    }
+}
